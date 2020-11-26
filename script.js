07b8711a77cf68b39a8c40106e9e0dd09b0f5029
Shahzad <shahzadrori@gmail.com>
2020-11-26 03:09:07 -0800

    making constants to deal in DOM

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-f
+ const main = document.getElementById('main');
+ const adduserbtn = document.getElementById('add-user');
+ const doublebtn = document.getElementById('double');
+ const shaowmillionairesbtn = document.getElementById('show-millionaires');
+ const sortbtn = document.getElementById('sort');
+ const calculatewealthbtn = document.getElementById('calculate-wealth');
+
+let data = [];
+
